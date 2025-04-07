b63289dfcb2e38c41856f7b943f8de97803652f4
pedrojesus <pedrojesus@PEDROJESUSA9A6>
2025-04-07 19:04:05 -0300

    Add project files.

---
+++ PJ.NavigationTransitions.Maui/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace PJ.NavigationTransitions.Maui
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ PJ.NavigationTransitions.Maui/Platforms/iOS/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace PJ.NavigationTransitions.Maui
+{
+    // All the code in this file is only included on iOS.
+    public class PlatformClass1
+    {
+    }
+}
