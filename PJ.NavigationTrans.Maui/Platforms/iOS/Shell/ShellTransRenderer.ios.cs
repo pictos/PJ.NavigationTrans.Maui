3c511e8247b5315dd67be46af07e124fd313976b
Pedro Jesus <pedrojesus.cefet@gmail.com>
2025-05-04 19:45:50 -0300

    code clean up

--- PJ.NavigationTrans.Maui/Platforms/iOS/Shell/ShellTransRenderer.ios.cs
+++ PJ.NavigationTrans.Maui/Platforms/iOS/Shell/ShellTransRenderer.ios.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Maui.Controls.Handlers.Compatibility;
+﻿using Microsoft.Maui.Controls.Handlers.Compatibility;
 using Microsoft.Maui.Controls.Platform.Compatibility;
 
 namespace PJ.NavigationTrans.Maui;
