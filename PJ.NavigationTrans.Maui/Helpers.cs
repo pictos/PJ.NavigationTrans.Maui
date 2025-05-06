d5be2ceb1b522b9e5561451ef5dd8f2fdb7434be
Pedro Jesus <pedrojesus.cefet@gmail.com>
2025-05-05 21:25:22 -0300

    Refactor AnimationRunnable to use WeakReference<T>
    Updated AnimationRunnable to utilize WeakReference<T> instead of a custom WeakWrapper<T> struct. Introduced an extension method, GetTargetOrDefault, for safer target retrieval. This change enhances code readability and maintainability by simplifying the implementation.

---
+++ PJ.NavigationTrans.Maui/Helpers.cs
@@ -0,0 +1,10 @@
+﻿namespace PJ.NavigationTrans;
+static class Helpers
+{
+	public static T? GetTargetOrDefault<T>(this WeakReference<T> weak)
+		where T : class
+	{
+		weak.TryGetTarget(out var target);
+		return target;
+	}
+}
--- PJ.NavigationTrans.Maui/Platforms/Android/AnimationRunnable.android.cs
+++ PJ.NavigationTrans.Maui/Platforms/Android/AnimationRunnable.android.cs
@@ -4,8 +4,8 @@ namespace PJ.NavigationTrans.Maui;
 
 sealed class AnimationRunnable : Java.Lang.Object, Java.Lang.IRunnable
 {
-	readonly WeakWrapper<Fragment> fragmentWrapper;
-	readonly WeakWrapper<AAnimation> animationWrapper;
+	readonly WeakReference<Fragment> fragmentWrapper;
+	readonly WeakReference<AAnimation> animationWrapper;
 	public Action<Fragment>? Finished { get; set; }
 
 	public AnimationRunnable(Fragment fragment, AAnimation animation)
@@ -16,12 +16,12 @@ sealed class AnimationRunnable : Java.Lang.Object, Java.Lang.IRunnable
 
 	public void Run()
 	{
-		var animation = animationWrapper.Target;
+		var animation = animationWrapper.GetTargetOrDefault();
 
 		if (animation is null)
 			return;
 
-		var fragment = fragmentWrapper.Target;
+		var fragment = fragmentWrapper.GetTargetOrDefault();
 
 		if (fragment is null)
 			return;
