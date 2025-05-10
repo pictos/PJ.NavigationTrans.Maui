e114f18479661e7d52a71e480d4e9b39ea8398c5
Pedro Jesus <pedrojesus.cefet@gmail.com>
2025-05-10 19:18:30 -0300

    Fixes into iOS NavigationRenderer to better support animations (#7)
    * Add built-in transition handling and update navigation
    
    - Introduced `IsBuiltIn` method in `AnimationHelpers.cs` to check for built-in transition types.
    - Updated `BuiltInAnimation` in `AnimationHelpers.ios.cs` to handle animations without adding `currentView` to the `window`.
    - Adjusted animation logic in `NavigationTransRenderer.cs` to utilize the new `IsBuiltIn` method.
    - Increased transition duration in `NewPage1.xaml.cs` from 500 to 4500 milliseconds.
    - Modified tap gesture handler in `NewPage2.xaml.cs` to check for Shell usage before navigating.
    - Created new solution file `PJ.NavigationTrans.Pjro.sln` for project structure and configurations.
    
    * Add OnPopViewAsync method to NavigationTransRenderer
    
    Implemented asynchronous view popping with animation in the
    NavigationTransRenderer class. Adjusted the insertion index
    for currentView in the window to improve view layering
    during transitions.
    
    * Refactor navigation animation handling
    
    - Updated `OnPopViewAsync` to return base method result after animation.
    - Replaced direct animation logic with `HandlePop()` and `HandlePush()` methods.
    - Improved code organization and readability by encapsulating animation logic.
    
    * pop animation is almost back
    
    * Improve view controller popping and remove MemoryTest class
    
    Updated the `NavigationTransRenderer` class to enhance view controller popping behavior by introducing an `ignorePopCall` flag and refining the `PopViewController` method. Simplified the `HandlePop` method by removing extraneous comments and code.
    
    Removed the `MemoryTest` static class and its associated methods, as it was deemed unnecessary for managing weak references to `UIView` objects.
    
    * Swap animation order in HandlePop method
    
    Reversed the order of animation calls in the
    `HandlePop` method of the `NavigationTransRenderer` class.
    This change updates the visual transition effect when
    popping a view in the navigation renderer.
    
    * update yml
    
    * fixed typo
    
    * dump change to trigger CI 🤞🏾
    
    * Add workflow_dispatch and environment variables
    
    This commit adds the `workflow_dispatch` event to both `dotnet-macos.yml` and `dotnet-windows.yml`, enabling manual triggering of workflows. It also defines the environment variables `LIB_CSPROJ` and `SAMPLE_CSPROJ` for project file paths. Additionally, `dotnet-windows.yml` includes a new `NugetPackageVersion` variable to format the NuGet package version with the run number.
    
    * fixed typo

--- PJ.NavigationTrans.Maui/Platforms/iOS/NavigationPage/NavigationTransRenderer.cs
+++ PJ.NavigationTrans.Maui/Platforms/iOS/NavigationPage/NavigationTransRenderer.cs
@@ -5,6 +5,10 @@ namespace PJ.NavigationTrans.Maui;
 
 sealed class NavigationTransRenderer : NavigationRenderer
 {
+	// Today when the `PopViewCotoller` is called, the `OnPopViewAsync` is called as well causing issues with animation,
+	// for now we just return earlier like the base implementation does. In a future release maybe this is better handled in the base class.
+	bool ignorePopCall;
+
 	public override void PushViewController(UIViewController viewController, bool animated)
 	{
 		if (UnsafeAccessorClass.GetUnsafeCurrentPageProperty(this) is Page currentPage)
@@ -21,6 +25,7 @@ sealed class NavigationTransRenderer : NavigationRenderer
 
 	public override UIViewController PopViewController(bool animated)
 	{
+		ignorePopCall = true;
 		if (UnsafeAccessorClass.GetUnsafeCurrentPageProperty(this) is Page currentPage)
 		{
 			var fromUIView = VisibleViewController.View;
@@ -31,6 +36,23 @@ sealed class NavigationTransRenderer : NavigationRenderer
 		}
 
 		return base.PopViewController(animated);
+	}
+
+	protected override Task<bool> OnPopViewAsync(Page page, bool animated)
+	{
+		if (ignorePopCall)
+		{
+			ignorePopCall = false;
+			return Task.FromResult(true);
+		}
+
+		var fromUIView = VisibleViewController.View;
+
+		Assert(fromUIView is not null);
+
+		animated = CreateAndApplyAnimation(page, NavigationRequestType.Pop, fromUIView);
+
+		return base.OnPopViewAsync(page, animated);
 	}
 
 	bool CreateAndApplyAnimation(Page currentPage, NavigationRequestType navigationRequest, UIView currentView)
@@ -54,15 +76,43 @@ sealed class NavigationTransRenderer : NavigationRenderer
 
 		Assert(window is not null);
 
-		// Add the currentView into the view in order to animate it.
-		window.InsertSubview(currentView, 0);
-
 		view.Layer.RemoveAllAnimations();
 		currentView.Layer.RemoveAllAnimations();
 
-		currentView.SelectAndRunAnimation(fromAnimation, info.Duration, currentView.RemoveFromSuperview);
-		view.SelectAndRunAnimation(toAnimation, info.Duration);
+		// If we use the Built-In animations, we don't add the `currentView` to the `window`.
+		if (toAnimation.IsBuiltIn() || fromAnimation.IsBuiltIn())
+		{
+			view.SelectAndRunAnimation(toAnimation, info.Duration);
+			goto END;
+		}
 
+		return navigationRequest == NavigationRequestType.Pop ? HandlePop() : HandlePush();
+
+		END:
 		return false;
+
+
+		bool HandlePush()
+		{
+			window.InsertSubview(currentView, 0);
+			currentView.SelectAndRunAnimation(fromAnimation, info.Duration, () =>
+			{
+				currentView.RemoveFromSuperview();
+			});
+			view.SelectAndRunAnimation(toAnimation, info.Duration);
+			return false;
+		}
+
+
+		// This causes a black background, not sure why it happens, but it is not a big issue.
+		bool HandlePop()
+		{
+			window.InsertSubview(view, 0);
+
+			view.SelectAndRunAnimation(toAnimation, info.Duration);
+			currentView.SelectAndRunAnimation(fromAnimation, info.Duration);
+
+			return false;
+		}
 	}
 }
