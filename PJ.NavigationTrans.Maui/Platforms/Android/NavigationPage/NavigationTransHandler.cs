eec68e5de676d8e15dcf76b3d7491625de98f6ab
Pedro Jesus <pedro.pj.souza@hotmail.com>
2025-05-11 19:41:57 -0300

    Add animation handling and refactor shell navigation
    - Updated `Directory.Build.props` to skip validation of MAUI implicit package references.
    - Introduced `AnimationRunnable` class for managing fragment animations.
    - Refactored `ShellTransItemRenderer` to handle fragment updates and navigation sources.
    - Enhanced `ShellTransRenderer` to manage fragment transitions with animations.
    - Removed `<SkipValidateMauiImplicitPackageReferences>` from the sample project file.
    - Added `NavigationTransHandler` class for future custom navigation handling.

---
+++ PJ.NavigationTrans.Maui/Platforms/Android/NavigationPage/NavigationTransHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+
+namespace PJ.NavigationTrans.Platforms.Android.NavigationPage;
+
+class NavigationTransHandler : NavigationViewHandler
+{
+	public NavigationTransHandler()
+	{
+	}
+}
+
+
+class X : NavigationViewFragment
+{
+	public override AAnimation OnCreateAnimation(int transit, bool enter, int nextAnim)
+	{
+		return base.OnCreateAnimation(transit, enter, nextAnim);
+	}
+}
